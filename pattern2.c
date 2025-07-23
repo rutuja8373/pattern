11f6fa66ec130c58960eb9ae1509cefdfd601f49
Rutuja Dhatrak <137909385+rutuja8373@users.noreply.github.com>
2025-07-23 10:55:19 +0530

    Add files via upload

---
+++ pattern2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if(i==1||i==5||j==1||j==5)
+			{
+				printf("*");
+			}
+			else
+			{ 
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+}
